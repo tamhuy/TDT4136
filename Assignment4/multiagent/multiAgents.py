852219b0ca28ba505f8815c28d31a29c5e0518a4
Quang-Tam Huynh <quangtamh95@gmail.com>
2016-10-16 17:29:32 +0200

    missing last test

--- Assignment4/multiagent/multiAgents.py
+++ Assignment4/multiagent/multiAgents.py
@@ -147,9 +147,9 @@ class MinimaxAgent(MultiAgentSearchAgent):
                 v = MIN(v, MAX-VALUE(RESULT(s,a)))
             return v
         """
-        print "-" * 50
-        print "Depth: ", self.depth
-        print "Total Agents: ", gameState.getNumAgents()
+        # print "-" * 50
+        # print "Depth: ", self.depth
+        # print "Total Agents: ", gameState.getNumAgents()
 
         # print gameState.getLegalActions()[0]
         # print gameState.generateSuccessor(1,"Left").getLegalActions(0)
@@ -163,7 +163,7 @@ class MinimaxAgent(MultiAgentSearchAgent):
         # print v[1]
         # return action
 
-        minimaxValue = self.maxValue(gameState, self.depth*gameState.getNumAgents())
+        minimaxValue = self.minimaxDecision(gameState, self.depth*gameState.getNumAgents())
         # print "Minimax value: ", minimaxValue
         return minimaxValue[1]
         # return None
@@ -186,32 +186,37 @@ class MinimaxAgent(MultiAgentSearchAgent):
         if depth == 0 or gameState.isWin() or gameState.isLose():
             return (scoreEvaluationFunction(gameState), None)
 
-        if depth % gameState.getNumAgents() == 0:
-            self.maxValue(gameState, depth)
+        if (depth % gameState.getNumAgents()) == 0:
+            return self.maxValue(gameState, depth)
+
         else:
-            self.minValue(gameState, depth)
+            return self.minValue(gameState, depth)
 
     def maxValue(self, gameState, depth):
-        if depth == 0 or gameState.isWin() or gameState.isLose():
-            print scoreEvaluationFunction(gameState)
-            return (scoreEvaluationFunction(gameState), None)
+        # if depth == 0 or gameState.isWin() or gameState.isLose():
+        #     print scoreEvaluationFunction(gameState)
+        #     return (scoreEvaluationFunction(gameState), None)
+        if len(gameState.getLegalActions(0)) == 0:
+            return scoreEvaluationFunction(gameState)
         v = (float('-inf'), None)
 
         for a in gameState.getLegalActions(0):
-            # v1 = self.minimaxDecision(gameState.generateSuccessor(0, a), depth - 1)
-            v1 = self.minValue(gameState.generateSuccessor(0, a), depth - 1)
+            v1 = self.minimaxDecision(gameState.generateSuccessor(0, a), depth - 1)
+            # v1 = self.minValue(gameState.generateSuccessor(0, a), depth - 1)
             if v1[0] > v[0]:
                 v = (v1[0], a)
         return v
 
     def minValue(self, gameState, depth):
-        if depth == 0 or gameState.isWin() or gameState.isLose():
-            return (scoreEvaluationFunction(gameState), None)
+        # if depth == 0 or gameState.isWin() or gameState.isLose():
+        #     return (scoreEvaluationFunction(gameState), None)
+        if len(gameState.getLegalActions(0)) == 0:
+            return scoreEvaluationFunction(gameState)
         v = (float('inf'), None)
 
         for a in gameState.getLegalActions(1):
-            # v1 = self.minimaxDecision(gameState.generateSuccessor(1, a), depth - 1)
-            v1 = self.maxValue(gameState.generateSuccessor(1, a), depth - 1)
+            v1 = self.minimaxDecision(gameState.generateSuccessor(1, a), depth - 1)
+            # v1 = self.maxValue(gameState.generateSuccessor(1, a), depth - 1)
             if v1[0] < v[0]:
                 v = (v1[0], a)
         return v
